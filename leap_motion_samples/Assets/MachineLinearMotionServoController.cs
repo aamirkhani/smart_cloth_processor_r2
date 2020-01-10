1243e2a8bda9bd9a297d832b5d5185c6c7a8cffe
Aame Khani <a.khani@samsung.com>
2020-01-10 15:42:26 -0800

    Servo 102 & 103 implemented to support z-axis translations/slidings

--- leap_motion_samples/Assets/MachineLinearMotionServoController.cs
+++ leap_motion_samples/Assets/MachineLinearMotionServoController.cs
@@ -27,11 +27,24 @@ public class MachineLinearMotionServoController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var slider_width = GameObject.Find("(static) SPACE_MARKER (slider-width marker)").gameObject.transform.localPosition.magnitude;
-        var slider_max_shift = GameObject.Find("(static) SPACE_MARKER (maximum translation marker)").gameObject.transform.localPosition.magnitude;
 
-        GameObject.Find("(slidable) SPACE_MARKER (SliderX_A holder)").gameObject.transform.localPosition = new Vector3(Servo100_XAxis_Channe1 * slider_max_shift, 0, 0);
-        GameObject.Find("(slidable) SPACE_MARKER (SliderX_B holder)").gameObject.transform.localPosition = new Vector3(Servo101_XAxis_Channe2 * slider_max_shift, 0, 0);
+        // X AXIS
+        var xaxis_slider_width = GameObject.Find("(static) SPACE_MARKER (slider-width marker)").gameObject.transform.localPosition.magnitude;
+        var xaxis_slider_max_shift = GameObject.Find("(static) SPACE_MARKER (maximum translation marker)").gameObject.transform.localPosition.magnitude;
+        GameObject.Find("(slidable) SPACE_MARKER (SliderX_A holder)").gameObject.transform.localPosition = new Vector3(Servo100_XAxis_Channe1 * xaxis_slider_max_shift, 0, 0);
+        GameObject.Find("(slidable) SPACE_MARKER (SliderX_B holder)").gameObject.transform.localPosition = new Vector3(Servo101_XAxis_Channe2 * xaxis_slider_max_shift, 0, 0);
+
+
+        // Z AXIS
+        var zaxis_slider_max_shift = GameObject.Find("(static) SPACE_MARKER (maximum translation marker) Z-Axis").gameObject.transform.localPosition;
+        GameObject.Find("(slidable) SPACE_MARKER (SliderZ_A holder)").gameObject.transform.localPosition = zaxis_slider_max_shift * Servo102_ZAxis_Channe1;
+        GameObject.Find("(slidable) SPACE_MARKER (SliderZ_B holder)").gameObject.transform.localPosition = zaxis_slider_max_shift * Servo103_ZAxis_Channe2;
+        
+
+
+
+
+
 
     }
 }
